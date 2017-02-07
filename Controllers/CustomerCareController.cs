b8af65ab2e8b9ca188ac4be7fc39afce694878fc
mseltzer19 <seltzer.19@buckeyemail.osu.edu>
2017-02-06 19:08:47 -0500

    update

--- Controllers/CustomerCareController.cs
+++ Controllers/CustomerCareController.cs
@@ -1,17 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 
-[Route("customer")]
+[Route("claims")]
 
 public class CustomerCareController : Controller
 {
     public IActionResult Index()
     {
         return Ok("This is the index of the CustomerCareController");
-    }
-
-    public IActionResult Claims()
-    {
-        return Ok("This is the claims of the CustomerCareController");
     }
 
   [Route("fileclaim")]
